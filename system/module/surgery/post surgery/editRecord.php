872da71bb1273e08ea7874d451472ac42c4e98b9
nishenp <nishen.peiris@syscolabs.com>
2017-12-05 06:39:11 +0530

    Complete edit record of post surgery

--- system/module/surgery/post surgery/editRecord.php
+++ system/module/surgery/post surgery/editRecord.php
@@ -5,3 +5,252 @@
  * Date: 12/4/17
  * Time: 9:34 AM
  */
+include_once "../../../../system/common/session_handling.php";
+include_once "../../../../system/common/dbconnection_inc.php";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // get data from post
+    $id = $_POST{'id'};
+    $BHT_no = $_POST['BHT_no'];
+    $photograph = $_POST['photograph'];
+    $complications = $_POST['complications'];
+    $post_surgery_investigations = $_POST['post_surgery_investigations'];
+    $date_of_discharge = $_POST['date_of_discharge'];
+    $discharge_plan = $_POST['discharge_plan'];
+
+    $sql = "UPDATE post_surgery set BHT_no = '$BHT_no', photograph = '$photograph', complications = '$complications', post_surgery_investigations = '$post_surgery_investigations', date_of_discharge = '$date_of_discharge', discharge_plan = '$discharge_plan' WHERE id = '$id'";
+    if ($conn->query($sql) == TRUE) {
+        $alert = "Record updated.";
+    } else {
+        $alert = "An error occurred while updating the record.";
+    }
+    ?>
+    <script type="text/javascript">
+        window.alert("<?php echo $alert; ?>");
+        window.location.href = "postSurgeryInformation.php"
+    </script>
+    <?php
+    $conn->close();
+} else {
+    // get the id of the record to be edited
+    if (isset($_GET{'id'})) {
+        $id = $_GET{'id'};
+    } else {
+        die();
+    }
+
+    // get the record to be edited
+    $sql = "SELECT BHT_no, photograph, complications, post_surgery_investigations, date_of_discharge, discharge_plan from post_surgery WHERE id = '$id'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $BHT_no = $row['BHT_no'];
+        $photograph = $row['photograph'];
+        $complications = $row['complications'];
+        $post_surgery_investigations = $row['post_surgery_investigations'];
+        $date_of_discharge = $row['date_of_discharge'];
+        $discharge_plan = $row['discharge_plan'];
+    } else {
+        $conn->close();
+        die();
+    }
+}
+?>
+
+<html>
+<head>
+    <title>Automation of Plastic Surgery Unit at Lady Ridgeway Hospital</title>
+    <link rel="icon" href="../../../images/title.png"/>
+    <link rel="stylesheet" type="text/css"
+          href="../../../bootstrap/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type="text/css"
+          href="../../../css/mainlayout.css"/>
+    <script type="text/javascript" src="../../../js/jquery-1.8.3.min.js">
+    </script>
+    <script type="text/javascript">
+        //Ajax for role Rights
+        function showRights(str) {
+            var xmlhttp;
+            if (str == "") {
+                document.getElementById("showrights").innerHTML = "";
+                return;
+            }
+            if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+                xmlhttp = new XMLHttpRequest();
+            }
+            else {// code for IE6, IE5
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    document.getElementById("showrights").innerHTML = xmlhttp.responseText;
+                }
+            }
+            xmlhttp.open("GET", "getRights.php?q=" + str, true);
+            xmlhttp.send();
+        }
+    </script>
+</head>
+<body>
+<div id="main">
+    <div id="header">
+        <?php include '../../../common/header.php'; ?>
+    </div>
+    <div id="navi">
+        <?php include '../../../common/navi.php'; ?>
+        &nbsp;
+    </div>
+    <div id="contents">
+        <div>
+            <ol class="breadcrumb">
+                <li>
+                    <a href="../../login/dashboard.php">
+                        Dashboard </a></li>
+                <li><a href="../../surgery/surgery.php">Surgery</a></li>
+                <li class="active"><a href="../../surgery/surgery information/surgeryInformation.php">Surgery</a></li>
+                <li class="font1" class="active">Post Surgery Information</li>
+            </ol>
+            <h3 align="center">Edit
+                Post Surgery Information
+            </h3>
+        </div>
+        <HR/>
+        <div>
+            <!-------------------- Post-surgery information form -------------------->
+            <form name="PostSurgeryInformationEdit" method="post" action="editRecord.php"
+                  enctype="multipart/form-data">
+                <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+                <div class="row">
+                    <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+                    <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+                    <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+                    <div class="col-lg-2 col-sm-2 col-md-2">
+                        BHT NO :
+                    </div>
+                    <div class="col-lg-3 col-sm-3 col-md-3">
+                        <input id="BHT_no" name="BHT_no" placeholder="BHT NO" class="form-control" required="required"
+                               value="<?php echo $BHT_no; ?>"/>
+                    </div>
+                    <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+                    <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+                    <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+                </div>
+        </div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="row">
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                Photograph after Surgery :
+            </div>
+            <div class="col-lg-3 col-sm-3 col-md-3">
+                <input id="photograph" name="photograph" type="file"
+                       placeholder="Image" class="form-control"/>
+            </div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+        </div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="row">
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                Complications :
+            </div>
+            <div class="col-lg-3 col-sm-3 col-md-3">
+                <input id="complications" name="complications" placeholder="Comment" class="form-control"
+                       required="required" value="<?php echo $complications; ?>"/>
+            </div>
+            <div class="col-lg-1 col-sm-1 col-md-1"><b>(graft)</b></div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+        </div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="row">
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                Post Surgery Investigations :
+            </div>
+            <div class="col-lg-3 col-sm-3 col-md-3">
+                <input id="post_surgery_investigations" name="post_surgery_investigations" placeholder="Comment"
+                       class="form-control" required="required" value="<?php echo $post_surgery_investigations; ?>"/>
+            </div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+        </div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="row">
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                Date of Discharge :
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                <input id="date_of_discharge" name="date_of_discharge" type="date" placeholder="DOB"
+                       class="form-control" required="required" value="<?php echo $date_of_discharge; ?>"/>
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+        </div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="row">
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                Discharge Plan :
+            </div>
+            <div class="col-lg-3 col-sm-3 col-md-3">
+                <input id="discharge_plan" name="discharge_plan" placeholder="Comment" class="form-control"
+                       required="required" value="<?php echo $discharge_plan; ?>"/>
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+        </div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="col-lg-12 col-sm-12 col-md-12"> &nbsp;</div>
+        <div class="row">
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                &nbsp;
+            </div>
+            <div class="col-lg-3 col-sm-3 col-md-3">
+                <button type="submit" class="btn btn-primary">
+                    <i class="glyphicon glyphicon-save"></i>
+                    Edit
+                </button>
+            </div>
+            <div class="col-lg-2 col-sm-2 col-md-2">
+                &nbsp;
+            </div>
+            <div class="col-lg-3 col-sm-3 col-md-3">
+                <button type="reset" class="btn btn-primary">
+                    <i class="glyphicon glyphicon-refresh"></i>
+                    Reset
+                </button>
+            </div>
+            <div class="col-lg-1 col-sm-1 col-md-1">&nbsp;</div>
+        </div>
+        </form>
+    </div>
+</div>
+<div id="footer">
+    <?php include '../../../common/footer.php'; ?>
+</div>
+</body>
+</html>
