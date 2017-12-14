6f7c497637bbfd3dd5bb278c8e08ccc26bac685b
nishenp <nishen.peiris@syscolabs.com>
2017-12-14 06:13:43 +0530

    Guardian information module complete without edit

---
+++ system/module/registration/guardian information/editRecord.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: root
+ * Date: 12/14/17
+ * Time: 5:34 AM
+ */
---
+++ system/module/registration/guardian information/guardianInformationProcess.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Nishen Peiris
+ * Date: 12/14/17
+ * Time: 5:35 AM
+ */
+
+/******************** All the field names of the guardian_information relation **************************/
+//id
+//clinic_no_type
+//clinic_no
+//BHT_no
+//guardian_name
+//address
+//gender
+//contact_no
+//occupation
+
+include_once "../../../../system/common/session_handling.php";
+include_once "../../../../system/common/dbconnection_inc.php";
+
+// initialize all the variables
+$id = $clinic_no_type = $clinic_no = $BHT_no = $guardian_name = $address = $gender = $contact_no = $occupation = "";
+
+// retrieve data from the post request
+$clinic_no_type = $_POST['clinic_no_type'];
+$clinic_no = $_POST['clinic_no'];
+$BHT_no = $_POST['BHT_no'];
+$guardian_name = $_POST['guardian_name'];
+$address = $_POST['address'];
+$gender = $_POST['gender'];
+$contact_no = $_POST['contact_no'];
+$occupation = $_POST['occupation'];
+
+$sql = "INSERT INTO guardian_information " .
+    "(clinic_no_type, clinic_no, BHT_no, guardian_name, address, gender, contact_no, occupation) " .
+    "VALUES ('$clinic_no_type', '$clinic_no', '$BHT_no', '$guardian_name', '$address', '$gender', '$contact_no', '$occupation');";
+if ($conn->query($sql) === TRUE) {
+    $alert = "New record created successfully";
+} else {
+    $alert = "Error: " . $conn->error . $sql;
+}
+$conn->close();
+?>
+
+<script>
+    window.alert("<?php echo $alert; ?>");
+    window.location.href = "guardianInformation.php"
+</script>
